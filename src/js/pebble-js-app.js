be3941eabd930d922b4d64895941eb40c37e253c
ebirtaid <diatribe@archlinux.us>
2015-06-25 21:13:53 -0400

    initial

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,77 @@
+var xhrRequest = function (url, type, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.send();
+};
+
+function locationSuccess(pos) {
+  // Construct URL
+  var api_key = "89f6a5a4d5fffaa1f02b6ca9c9c558f7";
+  var url = "https://api.forecast.io/forecast/" + api_key + "/" +
+      pos.coords.latitude + "," + pos.coords.longitude;
+
+  // Send request to OpenWeatherMap
+  xhrRequest(url, 'GET', 
+    function(responseText) {
+      // responseText contains a JSON object with weather info
+      var json = JSON.parse(responseText);
+
+      // Temperature in Kelvin requires adjustment
+      var temperature = json.currently.apparentTemperature;
+      console.log("Temperature is " + temperature);
+
+      // Conditions
+      var conditions = json.currently.summary;      
+      console.log("Conditions are " + conditions);
+      
+      // Assemble dictionary using our keys
+      var dictionary = {
+        "KEY_TEMPERATURE": temperature,
+        "KEY_CONDITIONS": conditions
+      };
+
+      // Send to Pebble
+      Pebble.sendAppMessage(dictionary,
+        function(e) {
+          console.log("Weather info sent to Pebble successfully!");
+        },
+        function(e) {
+          console.log("Error sending weather info to Pebble!");
+        }
+      );
+    }      
+  );
+}
+
+function locationError(err) {
+  console.log("Error requesting location!");
+}
+
+function getWeather() {
+  navigator.geolocation.getCurrentPosition(
+    locationSuccess,
+    locationError,
+    {timeout: 15000, maximumAge: 60000}
+  );
+}
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    console.log("PebbleKit JS ready!");
+
+    // Get the initial weather
+    getWeather();
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+    console.log("AppMessage received!");
+    getWeather();
+  }                     
+);
